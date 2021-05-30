1ca46389a5731e25c8cb537b9176e0d8f88451a2
masaul <79826454+masaul@users.noreply.github.com>
2021-05-30 22:54:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,235 @@
+function nextButton1(){
+    var page1 = document.getElementById("page-1");
+    var page2 = document.getElementById("page-2");
+    if(page1.style.display === "block"){
+        page1.style.display = "none";
+        page2.style.display = "block";
+    }
+}
+function nextButton2(){
+    var page2 = document.getElementById("page-2");
+    var page3 = document.getElementById("page-3");
+    if(page2.style.display === "block"){
+        page2.style.display = "none";
+        page3.style.display = "block";
+    }
+}
+function backButton2(){
+    var page1 = document.getElementById("page-1");
+    var page2 = document.getElementById("page-2");
+    if(page1.style.display === "none"){
+        page1.style.display = "block";
+        page2.style.display = "none";
+    }
+}
+function nextButton3(){
+    var page3 = document.getElementById("page-3");
+    var page4 = document.getElementById("page-4");
+    if(page3.style.display === "block"){
+        page3.style.display = "none";
+        page4.style.display = "block";
+    }
+}
+function backButton3(){
+    var page2 = document.getElementById("page-2");
+    var page3 = document.getElementById("page-3");
+    if(page2.style.display === "none"){
+        page2.style.display = "block";
+        page3.style.display = "none";
+    }
+}
+function nextButton4(){
+    var page4 = document.getElementById("page-4");
+    var page5 = document.getElementById("page-5");
+    if(page4.style.display === "block"){
+        page4.style.display = "none";
+        page5.style.display = "block";
+    }
+}
+function backButton4(){
+    var page3 = document.getElementById("page-3");
+    var page4 = document.getElementById("page-4");
+    if(page3.style.display === "none"){
+        page3.style.display = "block";
+        page4.style.display = "none";
+    }
+}
+function nextButton5(){
+    var page5 = document.getElementById("page-5");
+    var page6 = document.getElementById("page-6");
+    if(page5.style.display === "block"){
+        page5.style.display = "none";
+        page6.style.display = "block";
+    }
+}
+function backButton5(){
+    var page4 = document.getElementById("page-4");
+    var page5 = document.getElementById("page-5");
+    if(page4.style.display === "none"){
+        page4.style.display = "block";
+        page5.style.display = "none";
+    }
+}
+function nextButton6(){
+    var page6 = document.getElementById("page-6");
+    var page7 = document.getElementById("page-7");
+    if(page6.style.display === "block"){
+        page6.style.display = "none";
+        page7.style.display = "block";
+    }
+}
+function backButton6(){
+    var page5 = document.getElementById("page-5");
+    var page6 = document.getElementById("page-6");
+    if(page5.style.display === "none"){
+        page5.style.display = "block";
+        page6.style.display = "none";
+    }
+}
+function nextButton7(){
+    var page7 = document.getElementById("page-7");
+    var page8 = document.getElementById("page-8");
+    if(page7.style.display === "block"){
+        page7.style.display = "none";
+        page8.style.display = "block";
+    }
+}
+function backButton7(){
+    var page6 = document.getElementById("page-6");
+    var page7 = document.getElementById("page-7");
+    if(page6.style.display === "none"){
+        page6.style.display = "block";
+        page7.style.display = "none";
+    }
+}
+function nextButton8(){
+    var page8 = document.getElementById("page-8");
+    var page9 = document.getElementById("page-9");
+    if(page8.style.display === "block"){
+        page8.style.display = "none";
+        page9.style.display = "block";
+    }
+}
+function backButton8(){
+    var page7 = document.getElementById("page-7");
+    var page8 = document.getElementById("page-8");
+    if(page7.style.display === "none"){
+        page7.style.display = "block";
+        page8.style.display = "none";
+    }
+}
+function nextButton9(){
+    var page9 = document.getElementById("page-9");
+    var page10 = document.getElementById("page-10");
+    if(page9.style.display === "block"){
+        page9.style.display = "none";
+        page10.style.display = "block";
+    }
+}
+function backButton9(){
+    var page8 = document.getElementById("page-8");
+    var page9 = document.getElementById("page-9");
+    if(page8.style.display === "none"){
+        page8.style.display = "block";
+        page9.style.display = "none";
+    }
+}
+function nextButton10(){
+    var page10 = document.getElementById("page-10");
+    var page11 = document.getElementById("page-11");
+    if(page10.style.display === "block"){
+        page10.style.display = "none";
+        page11.style.display = "block";
+    }
+}
+function backButton10(){
+    var page9 = document.getElementById("page-9");
+    var page10 = document.getElementById("page-10");
+    if(page9.style.display === "none"){
+        page9.style.display = "block";
+        page10.style.display = "none";
+    }
+}
+function nextButton11(){
+    var page11 = document.getElementById("page-11");
+    var page12 = document.getElementById("page-12");
+    if(page11.style.display === "block"){
+        page11.style.display = "none";
+        page12.style.display = "block";
+    }
+}
+function backButton11(){
+    var page10 = document.getElementById("page-10");
+    var page11 = document.getElementById("page-11");
+    if(page10.style.display === "none"){
+        page10.style.display = "block";
+        page11.style.display = "none";
+    }
+}
+function nextButton12(){
+    var page12 = document.getElementById("page-12");
+    var page13 = document.getElementById("page-13");
+    if(page12.style.display === "block"){
+        page12.style.display = "none";
+        page13.style.display = "block";
+    }
+}
+function backButton12(){
+    var page11 = document.getElementById("page-11");
+    var page12 = document.getElementById("page-12");
+    if(page11.style.display === "none"){
+        page11.style.display = "block";
+        page12.style.display = "none";
+    }
+}
+function nextButton13(){
+    var page13 = document.getElementById("page-13");
+    var page14 = document.getElementById("page-14");
+    if(page13.style.display === "block"){
+        page13.style.display = "none";
+        page14.style.display = "block";
+    }
+}
+function backButton13(){
+    var page12 = document.getElementById("page-12");
+    var page13 = document.getElementById("page-13");
+    if(page12.style.display === "none"){
+        page12.style.display = "block";
+        page13.style.display = "none";
+    }
+}
+function nextButton14(){
+    var page14 = document.getElementById("page-14");
+    var page15 = document.getElementById("page-15");
+    if(page14.style.display === "block"){
+        page14.style.display = "none";
+        page15.style.display = "block";
+    }
+}
+function backButton14(){
+    var page13 = document.getElementById("page-13");
+    var page14 = document.getElementById("page-14");
+    if(page13.style.display === "none"){
+        page13.style.display = "block";
+        page14.style.display = "none";
+    }
+}
+function backButton15(){
+    var page14 = document.getElementById("page-14");
+    var page15 = document.getElementById("page-15");
+    if(page14.style.display === "none"){
+        page14.style.display = "block";
+        page15.style.display = "none";
+    }
+}
+
+
+
+function showLevel(){
+    var level = document.getElementById("button-showHide");
+    if(level.style.display === "none"){
+        level.style.display = "block";
+    }else if(level.style.display === "block"){
+        level.style.display = "none";
+    }
+}
